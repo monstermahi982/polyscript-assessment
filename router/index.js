92c434efffc4931b7a9642398197d890e56d6437
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-11-10 11:53:43 +0530

    products router added

--- router/index.js
+++ router/index.js
@@ -1,8 +1,14 @@
 import express from 'express'
 const router = express.Router();
 import userController from '../controller/userController'
+import productController from '../controller/productController'
 
 router.post('/login', userController.login);
 router.post('/register', userController.register);
+router.get('/products', productController.getAll);
+router.get('/product/:id', productController.getOne);
+router.post('/product', productController.add);
+router.put('/product/:id', productController.update);
+router.delete('/product/:id', productController.delete);
 
 export default router
