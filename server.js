3fef1adfa784b21ba2920f9a73e22b51341d370f
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2021-11-10 19:18:42 +0530

    added table schema

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import { PORT } from './config'
 import cors from 'cors'
 import errorhandler from './middleware/errorHandler'
 import routes from './router'
+import knex from './database'
 
 const app = express()
 
@@ -10,5 +11,27 @@ app.use(cors({ origin: ['http://localhost:4200'] }))
 app.use(express.json())
 app.use('/api', routes);
 app.use(errorhandler);
+
+async function table() {
+    // user table
+    await knex.schema.createTableIfNotExists("users", function (table) {
+        table.increments(); // integer id
+        table.string('name', 50);
+        table.string('email', 100);
+        table.string('password', 100);
+    })
+    // product table
+    const data = await knex.schema.createTableIfNotExists("prods", function (table) {
+        table.increments(); // integer id
+        table.string('name', 50);
+        table.integer('price', 100);
+        table.integer('quantity', 100);
+        table.date('createdAt');
+        table.string('expiry', 100)
+    })
+    console.log(data);
+}
+
+// table();
 
 app.listen(PORT, () => console.log(`app running on ${PORT} port`))
